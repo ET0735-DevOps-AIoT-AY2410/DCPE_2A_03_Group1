91319b258d2bc3ed748969c01f2bdcc40eecf3d5
Shune Lai Hlaing Hmee <hlainghmees.23@ichat.sp.edu.sg>
2024-07-05 16:41:34 +0800

    changed gpio pin

--- alarm.py
+++ alarm.py
@@ -9,15 +9,15 @@ def init():
     GPIO.setmode(GPIO.BCM)  
     GPIO.setwarnings(False)
     GPIO.setup(24, GPIO.OUT)  #gpio24 for led
-    GPIO.setup(25, GPIO.OUT)  #gpio25 for buzzer
+    GPIO.setup(18, GPIO.OUT)  #gpio25 for buzzer
 
 def when_fire_detected(fireDetected):
     while fireDetected == True:
         GPIO.output(24,1) #on
-        GPIO.output(25,1)
+        GPIO.output(18,1)
         sleep(1)
         GPIO.output(24,0) #off
-        GPIO.output(25,0)
+        GPIO.output(18,0)
         sleep(1)
         #i think shld be 0.5hz..? im not sure tho 
 
