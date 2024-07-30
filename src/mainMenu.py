0d7d050deab9908b7e7d080634466893d221365f
bryrybry <bryanong.23@ichat.sp.edu.sg>
2024-07-31 02:37:51 +0800

    Added REQs comments on mainMenu.py

--- src/mainMenu.py
+++ src/mainMenu.py
@@ -1,3 +1,31 @@
+
+# REQ-01
+# When the System turns on, the following active detection menu will be displayed. Format temperature and light intensity to 2 SF. Constantly update the LCD to display the latest temperature and light intensity values. When scanning, LCD will display:
+#
+#	 Line 1 = “Scanning Now.”
+#	 Line 2 = “Temp:xx Light:xx” 
+
+# REQ-02
+# While in the main menu defined in REQ-01, if key # is entered in the keypad, enter the sensitivity adjustment menu below:
+#
+# If key * is entered in the keypad, terminate the sensitivity adjustment and return to main menu
+# 
+# xx represents the old temperature threshold and yy should constantly update to display the currently read temperature value from sensor. Display the following lines in LCD and store collected data: 
+# 
+# 	 Line 1 = “Old Temp: xx”
+#  	 Line 2 = “New thrshld: yy”
+
+# REQ-03	
+# While in the adjustment menu defined in REQ-02, if key # is entered in the keypad, enter the sensitivity adjustment menu below:
+# 
+# If key # is entered in the keypad, terminate the sensitivity adjustment and return to main menu 
+# 
+# xx represents the old light threshold and yy should constantly update to display the currently read light value from sensor. Display the following lines in LCD and store collected data:
+# 
+# 	 Line 1 = “Old Light: xx”
+# 	 Line 2 = “New thrshld: yy”
+# After entering # in the keypad, return to main menu in REQ-01.
+
 import time
 from threading import Thread
 import queue
@@ -46,7 +74,7 @@ def start(lcd):
         lcd.lcd_display_string("Sensors Scanning", 1)                                                      #Display Scanning & Temp/Light values
         lcd.lcd_display_string("Temp: 1" + "Light: 2" ,2)
 
-        keyvalue= shared_keypad_queue.get()
+        keyvalue = shared_keypad_queue.get()
 
         if(keyvalue == 0):                         #if keypad '0' pressed, switch to adjustment system             
             scanner = False
