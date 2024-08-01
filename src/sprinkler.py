46f7331030aacbeb46e7d11b2a319b0af0f88297
Shune Lai Hlaing Hmee <hlainghmees.23@ichat.sp.edu.sg>
2024-08-01 14:54:42 +0800

    1st test for sprinkler.py

--- src/sprinkler.py
+++ src/sprinkler.py
@@ -1,3 +1,33 @@
 import time
 from time import sleep
 import RPi.GPIO as GPIO
+
+def init():
+    GPIO.setmode(GPIO.BCM)  # choose BCM mode
+    GPIO.setwarnings(False)
+    GPIO.setup(26, GPIO.OUT)  # set GPIO 26 as output
+
+def set_servo_position(position):
+    PWM = GPIO.PWM(26,50)
+    position = (-10*position)/180 + 12
+    PWM.start(position)
+    sleep(0.05)
+    PWM.stop()
+
+def when_fire_detected(fire_detected):
+    if fire_detected == True:
+        set_servo_position(180)
+    else:    
+        set_servo_position(0)
+
+
+def main():
+    init()
+    fireIsDetected = True
+    when_fire_detected(fireIsDetected)
+    sleep(4)
+
+
+
+if __name__ =="__main__":
+    main() 
