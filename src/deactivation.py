9b6324d2fa32d67a6234447a7ee0aff395bc1e26
ani <anirudh77.23@ichat.sp.edu.sg>
2024-08-12 16:04:34 +0800

    fixing code

--- src/deactivation.py
+++ src/deactivation.py
@@ -3,6 +3,7 @@ from hal import hal_buzzer as buzzer
 import notification as notif
 import RPi.GPIO as GPIO
 import alarm as turnoff
+import threading
 
 def main():
     reader = rfid.init()
@@ -13,13 +14,15 @@ def main():
 def false_alarm(reader):
     id = reader.read_id_no_block()
     id = str(id)
+    print ("RFID id: " + id)
+    fireDetected = True
+    turnoff.when_fire_detected(fireDetected)
 
     if id != "NONE":
-        turnoff.when_fire_detected(False)
         print ("false alarm")
-        fireDetected = False
-        print (id)
-        notif.sendNotif(false_alarm, "")    
+        fireDetected = True
+        turnoff.when_fire_detected(fireDetected)
+        notif.sendNotif(false_alarm, "Singapore Polytechnic")    
     return fireDetected
     
 
