cc83cf582dc177366ccb45130ea87f60622d5bae
bry <bryanong.23@ichat.sp.edu.sg>
2024-08-16 13:38:41 +0800

    config.json now determines location and recipient email, removed location variable from function input

--- src/notification.py
+++ src/notification.py
@@ -1,8 +1,13 @@
+import json
 from threading import Thread
 import requests # for telegram bot 
 import smtplib # smtp service, for emailing
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+# Load configuration from JSON file
+with open('notif_config.json', 'r') as config_file:
+    config = json.load(config_file)
 
 messageHeader_dict = { # title text dictionary
     "fire" : "FIRE ALARM ACTIVATED",
@@ -17,42 +22,41 @@ messageContent_dict = { # content text dictionary
 
 def debug(): # for testing notifications ONLY, not to be used by other code
     selectionInput = input('1: Fire\n2: Help needed\n3: False Alarm\nWhat is the situation? ')
-    locationInput = input('What is the location? ')
     if (int(selectionInput) == 1): 
-        sendNotif("fire", locationInput)
+        sendNotif("fire")
     elif (int(selectionInput) == 2):
-        sendNotif("help", locationInput)
+        sendNotif("help")
     elif (int(selectionInput) == 3):
-        sendNotif("false_alarm", locationInput)
+        sendNotif("false_alarm")
     else:
         print("ERROR: Invalid Selection")
 
-def sendNotif(type, location): # calls detached thread
+def sendNotif(type): # calls detached thread
     global messageContent_dict
     global messageHeader_dict
     print("Sending Notification and Email...")
-    Thread(target=thread_sendNotif, args=(type, location)).start()
+    Thread(target=thread_sendNotif, args=(type,)).start()
 
-def thread_sendNotif(type, location):
-    telegramNotif(type, location)
-    emailNotif(type, location)
+def thread_sendNotif(type):
+    telegramNotif(type)
+    emailNotif(type)
 
-def telegramNotif(type, location): # sends message via telegram
+def telegramNotif(type): # sends message via telegram
     token = "7317584084:AAFmG-5ZwZwNfU8fAiypMp56qsEmmEDKy00"
     chatID = "5896827510"
-    message = f"{messageContent_dict[type]}\nLocation: {location}"
+    message = f"{messageContent_dict[type]}\nLocation: {config['system_location']}"
     url_sendMessage = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatID}&text={message}"
 
     requests.get(url_sendMessage).json() # sends msg
     print(f"{type} notification sent to SCDF_Bot on Telegram!")
 
-def emailNotif(type, location):
+def emailNotif(type):
     from_email = 'devops_group1_notifsys@outlook.com'
-    to_email = 'bryanong.23@ichat.sp.edu.sg'
+    to_email = config['recipient_email']
     password = 'group1_devops'
 
     subject = messageHeader_dict[type]
-    email_content = f"Dear SCDF,\n    {messageContent_dict[type]}\n    Location: {location}"
+    email_content = f"Dear SCDF,\n    {messageContent_dict[type]}\n    Location: {config['system_location']}"
     
     msg = MIMEMultipart()
     msg['From'] = from_email
